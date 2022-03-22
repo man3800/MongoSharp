43c0ff359e369789139ea5010fc59a092222c718
admin <admin@HNKDEV-NOTEBOOK>
2022-03-22 15:51:42 +0900

    Add project files.

---
+++ MongoSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("startup");
+            var collec = database.GetCollection<BsonDocument>("employee");
+
+            var document = new BsonDocument
+            {
+                {"name","alex" },
+                {"Age","28" },
+                {"Salary","12000" },
+                {"position","designer" }
+            };
+            collec.InsertOneAsync(document);
+            Console.Read();
+        }
+    }
+}
